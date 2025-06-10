7c281243bca32e0f6c0c43656f929e028c52709e
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2025-06-09 22:43:40 -0400

    #627 - remove conditionals, sorting, unused args

--- src/officehours_api/admin_filters.py
+++ src/officehours_api/admin_filters.py
@@ -1,17 +1,16 @@
 from django.contrib.admin import SimpleListFilter
 from django.contrib.auth.models import User
+from django.db.models import F
 
 from officehours_api.models import Queue
 
 
 class BaseActiveFilter(SimpleListFilter):
-    def lookups(self, request, _):
-        related_objects = self.get_objects()
-        return sorted(
-            [(obj.id, obj.name if hasattr(obj, 'name') else obj.username) for
-             obj in related_objects], key=lambda λ: λ[1])
+    def lookups(self, *args):
+        related_objects = self.get_objects().distinct().order_by('name')
+        return [(obj.id, obj.name) for obj in related_objects]
 
-    def queryset(self, request, queryset):
+    def queryset(self, _, queryset):
         if self.value():
             try:
                 obj_id = int(self.value())
@@ -30,11 +29,12 @@ class BaseActiveFilter(SimpleListFilter):
 
 class ActiveHosts(BaseActiveFilter):
     title = 'hosts'
-    parameter_name = 'hosts'
+    parameter_name = 'host'
     filter_key = 'hosts__id'
 
     def get_objects(self):
-        return User.objects.filter(queue__isnull=False).distinct()
+        return User.objects.annotate(name=F('username')).filter(
+            queue__isnull=False)
 
 
 class ActiveQueues(BaseActiveFilter):
@@ -43,4 +43,4 @@ class ActiveQueues(BaseActiveFilter):
     filter_key = 'queue__id'
 
     def get_objects(self):
-        return Queue.objects.filter(meeting__isnull=False).distinct()
+        return Queue.objects.filter(meeting__isnull=False)
